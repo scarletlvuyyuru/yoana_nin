69f0eba8eec96b9f1bebcd9a07b125de21c6c150
scarletlvuyyuru <slvuyyuru@hotmail.com>
2026-04-07 11:03:01 -0400

    canonical fix and cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,33 @@ import App from './App.tsx'
 import { ThemeProvider } from './context/ThemeContext.tsx'
 import './index.css'
 
+const TRACKING_QUERY_PARAMS = [
+  'e',
+  'fbclid',
+  'gclid',
+  'msclkid',
+  'ttclid',
+  'mc_cid',
+  'mc_eid'
+]
+
+const removeTrackingParams = () => {
+  const url = new URL(window.location.href)
+  const hadTrackingParam = TRACKING_QUERY_PARAMS.some((param) => url.searchParams.has(param))
+
+  if (!hadTrackingParam) {
+    return
+  }
+
+  TRACKING_QUERY_PARAMS.forEach((param) => url.searchParams.delete(param))
+  const cleanQuery = url.searchParams.toString()
+  const cleanUrl = `${url.pathname}${cleanQuery ? `?${cleanQuery}` : ''}${url.hash}`
+
+  window.history.replaceState({}, document.title, cleanUrl)
+}
+
+removeTrackingParams()
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HelmetProvider>
